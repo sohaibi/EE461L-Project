0987ec2e39d5b4e16cdb2fba25a243a8d70563bd
Yue Cheng <chengyue@utexas.edu>
2021-03-20 17:55:59 -0500

    fixed login and register page, but no pasword validation

--- Frontend/src/Components/Pages/Login.js
+++ Frontend/src/Components/Pages/Login.js
@@ -1,38 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css';
 
 
- class Login extends React.Component {
-    
+class Login extends React.Component {
+
     state = {
         username: '',
         pwd: '',
     }
 
-   
-   
+
+
     handleChange = (e) => {
-        const {name, value} = e.target
-        this.setState({[name]: value})
+        const { name, value } = e.target
+        this.setState({ [name]: value })
     }
 
     handleSubmit = (e) => {
         e.preventDefault()
     }
 
-   render() {
-    return(
-        <div className='div-login'>
-            <div className>
-                <form onSubmit = {this.handleSubmit}>
-                    <input type='Username' name='Username' placeholder='Username' required onChange={this.handleChange} />
-                    <input type='password' name='pwd' placeholder='password...' required onChange = {this.handleChange} /> 
-                    <button onSubmit = {this.handleSubmit}>Log In</button>
-                </form>
-            </div>   
-        </div>
-      )
-   }
- }
+    render() {
+        return (
+            <div className='div-login'>
+                <div className>
+                    <form onSubmit={this.handleSubmit}>
+                        <label for='username'>Username</label>
+                        <input id='username' type='username' name='username' placeholder='username' required onChange={this.handleChange} />
+                        <label for='password'>Password</label>
+                        <input id='password' type='password' name='pwd' placeholder='password' required onChange={this.handleChange} />
+                        <button onSubmit={this.handleSubmit}>Log In</button>
+                    </form>
+                    <Link to="/register"
+                        className="register">
+                        <button Link>Register</button>
+                    </Link>
 
- export default Login;
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Login;
