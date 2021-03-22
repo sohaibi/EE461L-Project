e7cd18bd0fbce7aaa787f17c6d8dbfbe44d0fb09
Sohaib Khan <31521675+sohaibi@users.noreply.github.com>
2021-03-22 16:48:51 -0500

    Pushing my information for the about section

--- Frontend/src/Components/Pages/Home.js
+++ Frontend/src/Components/Pages/Home.js
@@ -117,12 +117,11 @@ function Home() {
                     <div className="profile">
                         <div className="profile-pic">
                             <div className="profile-linked" 
-                                onClick={()=> window.open("https://www.linkedin.com/", "_blank")} />
+                                onClick={()=> window.open("https://www.linkedin.com/in/sohaib-khan-2022/", "_blank")} />
                         </div>
                         <div className="profile-desc">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                Turpis egestas pretium aenean pharetra magna ac placerat vestibulum. 
-                                Lacus laoreet non curabitur gravida arcu. </p>
+                            <p>My name is Sohaib Khan and I am a fourth-year undergraduate student at UT Austin. I am going to graduate on May 2022 and aim to become a software
+                               developer. I am also curious about linking mathematics towards programming.</p>
                         </div>
                     </div>
 
