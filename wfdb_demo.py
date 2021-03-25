a816d51d1444a6913d45df135ce34d785dcfd8b3
Yue Cheng <chengyue@utexas.edu>
2021-03-24 21:11:07 -0500

    edit wdfb_demo.py

--- wfdb_demo.py
+++ wfdb_demo.py
@@ -1,23 +1,35 @@
 from IPython.display import display
 import matplotlib.pyplot as plt
 # matplotlib inline
-import numpy as np
+# import numpy as np
 import os
-import shutil
-import posixpath
+# import shutil
+# import posixpath
 import json
 import wfdb
 
 
-# Can also read the same files hosted on PhysioNet https://physionet.org/content/challenge-2015/1.0.0
-# in the /training/ database subdirectory.
-# record2 = wfdb.rdrecord('a103l', pn_dir='challenge-2015/training/')
-# print(record2.record_name)
-# print(record2.n_sig)
+# # Can also read the same files hosted on PhysioNet https://physionet.org/content/challenge-2015/1.0.0
+# # in the /training/ database subdirectory.
+# # record2 = wfdb.rdrecord('a103l', pn_dir='challenge-2015/training/')
+# # print(record2.record_name)
+# # print(record2.n_sig)
 
-# Demo 2 - Read certain channels and sections of the WFDB record using the simplified 'rdsamp' function
-# which returns a numpy array and a dictionary. Show the data.
-# Can also read the same files hosted on Physionet
-[signals2, fields2] = wfdb.rdsamp('s0010_re', channels=[14, 0, 5, 10], sampfrom=100, sampto=15000, pn_dir='ptbdb/patient001/')
-display(signals)
-display(fields)
+# signal, fields=wfdb.rdsamp('100',channels=[0, 1], sampfrom=0, sampto=1500, pn_dir='mitdb/')
+# #mitdb数据库的是数据是两导联，格式是[650000,2]的数据，channels是选择读取哪一个导联的
+# print(len(signal))
+# print('signal:',signal)
+# print('fields:',fields)
+# plt.plot(signal)
+# plt.ylabel(fields['units'][0])
+# plt.legend([fields['sig_name'][0],fields['sig_name'][1]])
+# plt.show()
+
+
+
+
+dl_dir="D:\\Downloads"
+
+if __name__ == '__main__': #这一段必须要加
+    wfdb.dl_database('mitdb', dl_dir)
+    display(os.listdir(dl_dir))
