ef37ce360d7adac6d7e8718ac94d59f92b2880e4
Sohaib Khan <31521675+sohaibi@users.noreply.github.com>
2021-04-01 15:59:22 -0500

    Pushing more changes

--- Frontend/src/Components/Menu.js
+++ Frontend/src/Components/Menu.js
@@ -1,40 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Menu.css';
-
-function Menu() {
-
-    return (
-
-        <ul>
-            <li>
-                <Link to="/editProfile" className="menu_link">
-                    <div className="list_item_div">
-                        Account Settings
-                    </div>
-                </Link>
-            </li>
-            <li>
-                <Link to="/project" className="menu_link">
-                    <div className="list_item_div">
-                        My Projects
-                    </div>
-                </Link>
-
-            </li>
-            <li>
-                <Link to="/hardware" className="menu_link">
-                    <div className="list_item_div">
-                        Hardware
-                    </div>
-                </Link>
-            </li>
-        </ul>
-    );
-}
-
-=======
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Menu.css';
@@ -70,5 +33,4 @@ function Menu() {
     );
 }
 
->>>>>>> Stashed changes
 export default Menu;
