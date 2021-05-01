fed6b4c67c7c581b4c951c36f3d0b6d342c43707
Yue Cheng <chengyue@utexas.edu>
2021-05-01 15:30:00 -0500

    changed some styple on dataset page

--- Frontend/src/Components/Pages/Datasets.js
+++ Frontend/src/Components/Pages/Datasets.js
@@ -15,29 +15,29 @@ export default function Datasets() {
     //data_names hold the names of datasets formated to work properly in urls
     useEffect(() => {
 
-        fetch('/dataset_info').then(res=> res.json()).then(
-            data =>{
+        fetch('/dataset_info').then(res => res.json()).then(
+            data => {
                 setData_Names(data_names, []);
                 setData_Keys(data_keys, []);
                 var i = 0;
-                for(i=0; i<10; i++){
-                    if(i==6){
+                for (i = 0; i < 10; i++) {
+                    if (i == 6) {
                         i = 11;
                     }
                     var data_key = String(data[0][i]);
-                   
+
                     setData_Keys(data_keys => [...data_keys, data_key]);
                     var data_name = String(data[1][i]);
                     setDatasets(datasets => [...datasets, data_name]);
                     data_name = data_name.split(" ").join("-");
                     data_name = data_name.replace("/", "");
-                    data_name= data_name.replace("(", "");
+                    data_name = data_name.replace("(", "");
                     data_name = data_name.replace(")", "");
                     data_name = data_name.toLowerCase();
-                    
+
                     setData_Names(data_names => [...data_names, data_name]);
-                    if(i==11){
-                        i=6;
+                    if (i == 11) {
+                        i = 6;
                     }
 
                 }
@@ -49,16 +49,16 @@ export default function Datasets() {
     }, []);
 
     //The function used to generate the table, including the url links
-     async function renderTableData(id_table, id_button) {
+    async function renderTableData(id_table, id_button) {
         var myTable = document.getElementById(id_table);
         var button = document.getElementById(id_button);
         button.parentNode.removeChild(button);
-        for(var i = 0; i<10; i++){
-            var new_row = myTable.insertRow(i+1);
+        for (var i = 0; i < 10; i++) {
+            var new_row = myTable.insertRow(i + 1);
             var dataset_name = new_row.insertCell(0);
             var dataset_link = new_row.insertCell(1);
             dataset_name.innerHTML = String(datasets[i]);
-            var url = "https://www.physionet.org/static/published-projects/"+data_keys[i]+"/"+data_names[i]+"-1.0.0.zip";
+            var url = "https://www.physionet.org/static/published-projects/" + data_keys[i] + "/" + data_names[i] + "-1.0.0.zip";
             dataset_link.innerHTML = "<a id='id' href=''>Download</a>";
             var temp = dataset_link.getElementsByTagName('a')[0];
             temp.setAttribute("href", url);
@@ -66,7 +66,7 @@ export default function Datasets() {
             console.log(datasets[i]);
         }
         console.log(myTable.rows.length);
-     }
+    }
 
 
     //Returns table with dataset names and download buttons
@@ -76,7 +76,7 @@ export default function Datasets() {
             <p>  Select Datasets to download.</p> */}
             <h1 >How to Download Datasets</h1>
             <p>The datasets provided by our website are taken from a website called PhysioNet. They download in the form of a zip and once expanded contains
-                 .cvs, .hea, and .dat. As well as others. All screening of specific document data is done by PhysioNet. If you would like ot continue please press
+            .cvs, .hea, and .dat. As well as others. All screening of specific document data is done by PhysioNet. If you would like ot continue please press
                  the button below. </p>
             <table id="dynamic_table">
                 <thead>
@@ -93,8 +93,9 @@ export default function Datasets() {
 
                 </tbody>
             </table>
-            <button id="button" onClick={()=>renderTableData("dynamic_table", "button")}>Continue to Dataset Options</button>
-
+            <div>
+                <button id="button" onClick={() => renderTableData("dynamic_table", "button")}>Continue to Dataset Options</button>
+            </div>
         </div>
     )
 
